ef1e17f848d8048adc85427737ee1eb1541ce989
Rome44100 <rome44100@gmail.com>
2021-10-26 21:25:12 +0300

    Start Lesson 2. Complete exercise 1.

--- src/user.ts
+++ src/user.ts
@@ -1,4 +1,26 @@
-import { renderBlock } from './lib.js'
+import { renderBlock } from './lib.js';
+
+/**
+ * Get username and avatarUrl from localStorage
+ * @param {string} user 'user'
+ * @returns {unknown}
+ */
+export function getUserData (user = 'user'): unknown {
+  const data = JSON.parse(localStorage.getItem(user));
+  return { 
+    username: data.username, 
+    avatarUrl: data.avatarUrl
+  };
+}
+
+/**
+ * Get favourites amount
+ * @param {string} favoritesAmount
+ * @returns {unknown}
+ */
+export function getFavoritesAmount (favoritesAmount = 'favoritesAmount'):unknown {
+  return localStorage.getItem(favoritesAmount);
+}
 
 export function renderUserBlock (
   userName:string, 

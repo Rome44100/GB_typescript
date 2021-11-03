d7f82f9745407ba630d2ceb8bed8de52cd56e5a9
Rome44100 <rome44100@gmail.com>
2021-11-03 21:20:01 +0300

    Start exercise 1

--- src/sdk/flat-rent-sdk.d.ts
+++ src/sdk/flat-rent-sdk.d.ts
@@ -1,3 +1,9 @@
 export function cloneDate(date: Date): Date
 
 export function addDays(date: Date, days: number): Date
+
+export interface FlatRentSdk {
+    get(id: number): Promise<object|null>
+    search(parameters: object): object[]
+    book(flatId: number, checkInDate: Date, checkOutDate: Date): number
+}
